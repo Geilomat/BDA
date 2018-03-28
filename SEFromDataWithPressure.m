af5adc74e30e68504daccbee988a5aae36852069
Unknown <michikurmann@hotmail.com>
2018-03-28 16:30:59 +0200

    Started some new projects also added Pressure with different sampling

--- SEFromDataWithPressure.m
+++ SEFromDataWithPressure.m
@@ -12,7 +12,7 @@ load('PressLookUp.mat');
 
 A = [0 1 0 0 0 0;0 0 1 0 0 0; 0 0 0 0 0 0;0 PressLookUp(1,1) 0 0 0 PressLookUp(2,1);0 0 0 0 0 1;0 0 0 0 0 0];
 B = [0;0;0;0;0;0];                %No direct input
-C = [1 0 0 0 0 0;0 0 1 0 0 0;0 0 0 1 0 0;0 0 0 1 0 0;0 0 0 0 1 0];          %Output ist Height and Acceleration
+C = [0 0 1 0 0 0;0 0 0 1 0 0;0 0 0 1 0 0;0 0 0 0 1 0];          %Output ist Height and Acceleration
 D = [0;0];
 G = [0;0;1;1;0;1];                %System noise only on acceloration
 
@@ -52,22 +52,11 @@ legend('Real height in z');
 
 figure('Name','Real Data');
 % get acceloration by differentiate height:
-a = diff(diff(h)/Tau)/Tau;
+v = diff(h)/Tau;
+a = diff(v)/Tau;
 % Ad zeros to maintain vector length
+v = [v 0];
 a = [a 0 0];
-
-% get height of GPS by deleting engouh values so it becomes 5Hz sample rate
-% and then ad Zero Order Hold to get static value
-GPSTau = 1/5;
-h_GPS = zeros(1, length(TimeVec)-20);
-
-for k =  1:(length(h)/round(GPSTau/Tau))
-    for t =  1:round(GPSTau/Tau)
-        h_GPS((k-1)*round(GPSTau/Tau)+t) = h((k-1)*round(GPSTau/Tau)+1);
-   end
-end
-
-h_GPS = [h_GPS ones(1,20)*h_GPS(end)];
 
 % Get a Temperatur vector with dicreasing Tempratur depending on height
 T0 = 15 + 273.15;
@@ -79,63 +68,92 @@ T = T0 - 0.00649*h;
 Po = 1013.25;    %Pressure at altitude 0
 p = Po*(1-(0.0065*h)./T).^5.255;
 
-
+%P1 Has less noise Therefore samples slower
+P1Tau = 1/50;
+P1 = zeros(1,length(TimeVec));
+for k =  1:(length(p)/round(P1Tau/Tau))
+    for t =  1:round(P1Tau/Tau)
+        if (k-1)*round(P1Tau/Tau)+t <= length(TimeVec)
+        P1((k-1)*round(P1Tau/Tau)+t) = p((k-1)*round(P1Tau/Tau)+1);
+        end
+   end
+end
+                            
+%P2 Has more noise but samples faster
+P2Tau = 1/100;
+P2 = zeros(1,length(TimeVec));
+for k =  1:(length(p)/round(P2Tau/Tau))
+    for t =  1:round(P2Tau/Tau)
+        if (k-1)*round(P2Tau/Tau)+t <= length(TimeVec)
+        P2((k-1)*round(P2Tau/Tau)+t) = p((k-1)*round(P2Tau/Tau)+1);
+        end
+   end
+end                           
 
 plot(TimeVec,h);
 hold on;
-plot(TimeVec,h_GPS)
 plot(TimeVec,a);
 plot(TimeVec,T)
-plot(TimeVec,p);
-legend('Real height in z','GPS heigt in z','Real Acceloration','Assumed Temperature in Kelvin','Assumed Pressure');
+plot(TimeVec,P1);
+plot(TimeVec,P2);
+legend('Real height in z','Real Acceloration','Assumed Temperature in Kelvin','Assumed Pressure 1','Assumed Pressure 2');
 hold off;
 
 %% Add noise to sensor data
 
-T_mes = awgn(T,80,'measured');
-h_mes_GPS = awgn(h_GPS,100,'measured');
-p_mes_1 = awgn(p,35,'measured');
-p_mes_2 = awgn(p,40,'measured');
-a_mes = awgn(a,80,'measured');
+T_mes = awgn(T,40,'measured');
+p_mes_1 = awgn(p,40,'measured');
+p_mes_2 = awgn(p,35,'measured');
+a_mes = awgn(a,30,'measured');
 
 figure('Name','Noise Data');
 hold on;
-plot(h_mes_GPS);
 plot(p_mes_1);
 plot(p_mes_2);
 plot(a_mes);
 plot(T_mes);
-legend('GPS','Pressure 1','Pressure 2','Acceloration Measured','Temperatur');
+legend('Pressure 1','Pressure 2','Acceloration Measured','Temperatur');
 hold off;
 
-%% State estiamation initialize
+%% Create Noise Matrices
 
-% NoiseMatrices
-%Sensor noise
-GPS = 1;
-ACL = 0.1;
-BM1 = 5;
-BM2 = 3;
-TRM = 0.01;
-R = diag([GPS ACL BM1 BM2 TRM]);
-ZV = zeros(1,length(TimeVec));
+% Static Sensor noise
+ACLvar = 0.1;
+BM1var = 3;
+BM2var = 1;
+TRMvar = 0.01;
+R = diag([ACLvar BM1var BM2var TRMvar]);
+
 %Dynamic Sensor noise:
-GPSStep = [0.01 ones(1,round(GPSTau/Tau) - 1)*100000];
-GPS = [];
-for n = 1:round(length(TimeVec)/length(GPSStep))-1
-    GPS = [GPS GPSStep];
+ACLvar = ones(1,length(TimeVec))*0.1;
+
+BM1Step = [BM1var ones(1,round(P1Tau/Tau)-1)*2^32];
+BM1var = [];
+for n = 1:round(length(TimeVec)/length(BM1Step))-1
+    BM1var = [BM1var BM1Step];
 end
-GPS = [GPS ones(1,20)*1000];
-ACL = ones(1,length(TimeVec))*0.1;
-BM1 = ones(1,length(TimeVec))*5;
-BM2 = ones(1,length(TimeVec))*3;
-TRM = ones(1,length(TimeVec))*0.01;
-R_dyn = [GPS;ACL;BM1;BM2;TRM];
-R_dyn_t = timeseries(R_dyn_m,TimeVec);
+BM1var = [BM1var ones(1,length(TimeVec)-length(BM1var))*2^32];
+%BM1var = ones(1,length(TimeVec))*0.5;
+
+BM2Step = [BM2var ones(1,round(P2Tau/Tau)-1)*2^32];
+BM2var = [];
+for n = 1:round(length(TimeVec)/length(BM2Step))-1
+    BM2var = [BM2var BM2Step];
+end
+BM2var = [BM2var ones(1,length(TimeVec)-length(BM2var))*2^32];
+%BM2var = ones(1,length(TimeVec))*0.3;
+
+TRMvar = ones(1,length(TimeVec))*0.1;
+
+%Add all noise vectors into an noise matrix
+R_dyn = [ACLvar;BM1var;BM2var;TRMvar];
 R_dyn_m = diag(R_dyn(:,1)');
 for n = 2:length(TimeVec)
     R_dyn_m = cat(3,R_dyn_m,diag(R_dyn(:,n)'));
 end
+R_dyn_t = timeseries(R_dyn,TimeVec);
+
+
 %Static System noise:
 HGT = 0;
 SPE = 0;
@@ -143,7 +161,8 @@ ACEL = 70;
 PRE = 0.1;
 TMP = 0;
 DTMP = 0.1;
-Q = diag([HGT;SPE;ACEL;PRE;TMP;DTMP]);           
+Q = diag([HGT;SPE;ACEL;PRE;TMP;DTMP]);
+
 %Dynamic Sytem noise:
 HGT = ones(1,length(TimeVec))*0;
 SPE = ones(1,length(TimeVec))*0;
@@ -152,16 +171,22 @@ PRE = ones(1,length(TimeVec))*0.1;
 TMP = ones(1,length(TimeVec))*0;
 DTMP = ones(1,length(TimeVec))*0.1;
 Q_dyn = [HGT;SPE;ACEL;PRE;TMP;DTMP];  
-Q_dyn_t = timeseries(Q_dyn_m,TimeVec);
+
+%Add all noise vectors into an noise matrix
 Q_dyn_m = diag(Q_dyn(:,1)');
 for n = 2:length(TimeVec)
     Q_dyn_m = cat(3,Q_dyn_m,diag(Q_dyn(:,n)'));
 end
-% Initialize
+Q_dyn_t = timeseries(Q_dyn,TimeVec);
+
+
+
+%% Initialize
 u = zeros(1,length(TimeVec));                       %Input vector is zero
-y = [h_mes_GPS;a_mes;p_mes_1;p_mes_2;T_mes];        %Output are the measurements
+y = [a_mes;p_mes_1;p_mes_2;T_mes];        %Output are the measurements
 y_t = timeseries(y,TimeVec);
-x = [0;0;0;Po;T(1);0];                              %Is reality
+x0 = [0;0;0;Po;T(1);0];                             %Start points
+x =  x0;                   %Is reality
 P0 = eye(6);
 
 %% Excecute Simulation Static
@@ -184,26 +209,23 @@ ylabel('height & accelaration');
 xlabel('Time [s]');
 hold off;
 
-%% Excecute Simulation Unscentend
-
-
-
 %% Loop
-u = zeros(1,length(TimeVec));   %Input vector is zero
-y = [h_mes_GPS;a_mes;p_mes_1;p_mes_2;T_mes];                %Output are the measurements
+u = zeros(1,length(TimeVec));                       %Input vector is zero
+y = [a_mes;p_mes_1;p_mes_2;T_mes];                  %Output are the measurements
 y_t = timeseries(y,TimeVec);
-x = [0;0;0;Po;T(1);0];                    %Is reality
-P = eye(6);
+x = [0;0;0;Po;T(1);0];                              %Start Vector should be like this
+P = eye(6);                                         %Standart can maybe be increased
 
+x_est_loop = zeros(size(x,1),length(TimeVec));      %Vector for the SE values
 for k = 1:length(TimeVec)
     K = P*C'*pinv(C*P*C' + R_dyn_m(:,:,k));
     x = x + K*(y(:,k) - C*x);
     P = (eye(6)-K*C)*P;
     
-    x_est_loop(:,k) = x;  %Save data from the Sensor fusion
+    x_est_loop(:,k) = x;                            %Save data from the Sensor fusion
     
-    x = Ad*x + B*u(k);
-    P = Ad*P*Ad' + Q_dyn_m(:,:,k);%Gd*Q*Gd';
+    x = Ad*x + Bd*u(k);
+    P = Ad*P*Ad' + Q_dyn_m(:,:,k); %Gd*Q*Gd';
 
 end
 
@@ -214,11 +236,13 @@ grid on;
 hold on;
 plot(TimeVec,x_est_loop(1,:)); 
 plot(X_estimatd.time,X_estimatd.signals.values(:,1));
+plot(TimeVec,v);
+plot(TimeVec,x_est_loop(2,:));
 plot(TimeVec,a);
 plot(TimeVec,x_est_loop(3,:));
 plot(TimeVec,p);
 plot(TimeVec,x_est_loop(4,:));
 hold off;
-legend('real Height','estiamted Height','estimated Height Simulink','real acceloration','estimated acceloration','real pressure','estimated pressure');
+legend('real Height','estiamted Height','estimated Height Simulink','real Speed','estimated Speed','real acceloration','estimated acceloration','real pressure','estimated pressure');
 ylabel('height & accelaration');
 xlabel('Time [s]');
---
+++ SEFromDataWithPressureGPS.m
@@ -0,0 +1,274 @@
+% In this script the State Estimation from given Sensor Data or self produced should be
+% compared to the ground truth.
+
+clear all; close all;
+
+
+%% Initalize the System
+
+% x1 = Height, x2 = Velocity, x3 Acceloration, x4 Temp , x5 Pressure
+%Tempteratur at start = 15 C° -> 288.15 K°
+load('PressLookUp.mat');
+
+A = [0 1 0 0 0 0;0 0 1 0 0 0; 0 0 0 0 0 0;0 PressLookUp(1,1) 0 0 0 PressLookUp(2,1);0 0 0 0 0 1;0 0 0 0 0 0];
+B = [0;0;0;0;0;0];                %No direct input
+C = [1 0 0 0 0 0;0 0 1 0 0 0;0 0 0 1 0 0;0 0 0 1 0 0;0 0 0 0 1 0];          %Output ist Height and Acceleration
+D = [0;0];
+G = [0;0;1;1;0;1];                %System noise only on acceloration
+
+
+
+% Get the time vector from the ARIS simulation.
+load('TimeFromHassan.mat');
+TimeVec = t';
+Tau =  TimeVec(end)/length(TimeVec);
+
+
+ %Diskretierung der Systemmatritzen
+Ad = expm(A*Tau);     
+Gd = Ad * G;
+Bd = Ad * B;
+
+% Dummy vairable for Simualation
+simin = zeros(1,length(TimeVec));
+
+
+%% Observability test;
+rank([C;C*Ad;C*Ad*Ad])
+
+%% Read sensor Data
+load('StateFromHassan.mat');
+
+% Get the heigth vector from the ARIS simulation.
+h = state(:,3)';
+
+% plot it
+figure('Name','Real Data')
+plot(h,'b') %%Original bahn
+legend('Real height in z');
+
+
+%% Produce noisless sensor Data self
+
+figure('Name','Real Data');
+% get acceloration by differentiate height:
+v = diff(h)/Tau;
+a = diff(v)/Tau;
+% Ad zeros to maintain vector length
+v = [v 0];
+a = [a 0 0];
+
+
+% get height of GPS by deleting engouh values so it becomes 5Hz sample rate
+% and then ad Zero Order Hold to get static value
+GPSTau = 1;
+h_GPS = zeros(1, length(TimeVec));
+for k =  1:(length(h)/round(GPSTau/Tau))+1
+    for t =  1:round(GPSTau/Tau)
+        if (k-1)*round(GPSTau/Tau)+t <= length(TimeVec)
+        h_GPS((k-1)*round(GPSTau/Tau)+t) = h((k-1)*round(GPSTau/Tau)+1);
+        end
+   end
+end
+%h_GPS = [h_GPS ones(1,20)*h_GPS(end)];
+
+% Get a Temperatur vector with dicreasing Tempratur depending on height
+T0 = 15 + 273.15;
+T = T0 - 0.00649*h;
+
+
+% Get Barometric Data (pressure)
+% Pressure Data Temp/Po are just assumptions !!!!
+Po = 1013.25;    %Pressure at altitude 0
+p = Po*(1-(0.0065*h)./T).^5.255;
+
+%P1 Has less noise Therefore samples slower
+P1Tau = 1/50;
+P1 = zeros(1,length(TimeVec));
+for k =  1:(length(p)/round(P1Tau/Tau))+1
+    for t =  1:round(P1Tau/Tau)
+        if (k-1)*round(P1Tau/Tau)+t <= length(TimeVec)
+        P1((k-1)*round(P1Tau/Tau)+t) = p((k-1)*round(P1Tau/Tau)+1);
+        end
+   end
+end
+                            
+%P2 Has more noise but samples faster
+P2Tau = 1/100;
+P2 = zeros(1,length(TimeVec));
+for k =  1:(length(p)/round(P2Tau/Tau))+1
+    for t =  1:round(P2Tau/Tau)
+        if (k-1)*round(P2Tau/Tau)+t <= length(TimeVec)
+        P2((k-1)*round(P2Tau/Tau)+t) = p((k-1)*round(P2Tau/Tau)+1);
+        end
+   end
+end                           
+
+plot(TimeVec,h);
+hold on;
+plot(TimeVec,h_GPS)
+plot(TimeVec,a);
+plot(TimeVec,T)
+plot(TimeVec,P1);
+plot(TimeVec,P2);
+legend('Real height in z','GPS heigt in z','Real Acceloration','Assumed Temperature in Kelvin','Assumed Pressure 1','Assumed Pressure 2');
+hold off;
+
+%% Add noise to sensor data
+
+T_mes = awgn(T,40,'measured');
+h_mes_GPS = awgn(h_GPS,50,'measured');
+p_mes_1 = awgn(p,40,'measured');
+p_mes_2 = awgn(p,35,'measured');
+a_mes = awgn(a,30,'measured');
+
+figure('Name','Noise Data');
+hold on;
+plot(h_mes_GPS);
+plot(p_mes_1);
+plot(p_mes_2);
+plot(a_mes);
+plot(T_mes);
+legend('GPS','Pressure 1','Pressure 2','Acceloration Measured','Temperatur');
+hold off;
+
+%% Create Noise Matrices
+
+% Static Sensor noise
+GPSvar = 0.1;
+ACLvar = 0.3;
+BM1var = 5;
+BM2var = 3;
+TRMvar = 0.01;
+R = diag([GPSvar ACLvar BM1var BM2var TRMvar]);
+ZV = zeros(1,length(TimeVec));
+
+%Dynamic Sensor noise:
+GPSStep = [GPSvar ones(1,round(GPSTau/Tau) - 1)*2^32];
+GPSvar = [];
+for n = 1:round(length(TimeVec)/length(GPSStep))-1
+    GPSvar = [GPSvar GPSStep];
+end
+GPSvar = [GPSvar ones(1,length(TimeVec)-length(GPSvar))*2^32];
+
+ACLvar = ones(1,length(TimeVec))*0.1;
+
+BM1Step = [BM1var ones(1,round(P1Tau/Tau)-1)*2^32];
+BM1var = [];
+for n = 1:round(length(TimeVec)/length(BM1Step))-1
+    BM1var = [BM1var BM1Step];
+end
+BM1var = [BM1var ones(1,length(TimeVec)-length(BM1var))*2^32];
+%BM1var = ones(1,length(TimeVec))*0.5;
+
+BM2Step = [BM2var ones(1,round(P2Tau/Tau)-1)*2^32];
+BM2var = [];
+for n = 1:round(length(TimeVec)/length(BM2Step))-1
+    BM2var = [BM2var BM2Step];
+end
+BM2var = [BM2var ones(1,length(TimeVec)-length(BM2var))*2^32];
+%BM2var = ones(1,length(TimeVec))*0.3;
+
+TRMvar = ones(1,length(TimeVec))*0.1;
+
+%Add all noise vectors into an noise matrix
+R_dyn = [GPSvar;ACLvar;BM1var;BM2var;TRMvar];
+R_dyn_m = diag(R_dyn(:,1)');
+for n = 2:length(TimeVec)
+    R_dyn_m = cat(3,R_dyn_m,diag(R_dyn(:,n)'));
+end
+R_dyn_t = timeseries(R_dyn,TimeVec);
+
+
+%Static System noise:
+HGT = 0;
+SPE = 0;
+ACEL = 70;
+PRE = 0.1;
+TMP = 0;
+DTMP = 0.1;
+Q = diag([HGT;SPE;ACEL;PRE;TMP;DTMP]);
+
+%Dynamic Sytem noise:
+HGT = ones(1,length(TimeVec))*0;
+SPE = ones(1,length(TimeVec))*0;
+ACEL = [100 100 100 50 30 ones(1,length(TimeVec)-5)*20];
+PRE = ones(1,length(TimeVec))*0.1;
+TMP = ones(1,length(TimeVec))*0;
+DTMP = ones(1,length(TimeVec))*0.1;
+Q_dyn = [HGT;SPE;ACEL;PRE;TMP;DTMP];  
+
+%Add all noise vectors into an noise matrix
+Q_dyn_m = diag(Q_dyn(:,1)');
+for n = 2:length(TimeVec)
+    Q_dyn_m = cat(3,Q_dyn_m,diag(Q_dyn(:,n)'));
+end
+Q_dyn_t = timeseries(Q_dyn,TimeVec);
+
+
+
+%% Initialize
+u = zeros(1,length(TimeVec));                       %Input vector is zero
+y = [h_mes_GPS;a_mes;p_mes_1;p_mes_2;T_mes];        %Output are the measurements
+y_t = timeseries(y,TimeVec);
+x0 = [0;0;0;Po;T(1);0];                             %Start points
+x =  x0;                   %Is reality
+P0 = eye(6);
+
+%% Excecute Simulation Static
+disp('Simulation start..');
+sim('RocketSE');
+disp('...finished!');
+
+%% Plot
+figure('Name','Real flight vs estimation simulink');
+plot(TimeVec,h);
+hold on;
+grid on;
+plot(X_estimatd.time,X_estimatd.signals.values(:,1));
+plot(TimeVec,a)
+plot(X_estimatd.time,X_estimatd.signals.values(:,3));
+plot(TimeVec,p)
+plot(X_estimatd.time,X_estimatd.signals.values(:,4));
+legend('real Height','estiamted Height','real acceloration','estimated acceloration','real pressure','estimated pressure');
+ylabel('height & accelaration');
+xlabel('Time [s]');
+hold off;
+
+%% Loop
+u = zeros(1,length(TimeVec));                       %Input vector is zero
+y = [h_mes_GPS;a_mes;p_mes_1;p_mes_2;T_mes];        %Output are the measurements
+y_t = timeseries(y,TimeVec);
+x = [0;0;0;Po;T(1);0];                              %Start Vector should be like this
+P = eye(6);                                         %Standart can maybe be increased
+
+x_est_loop = zeros(size(x,1),length(TimeVec));      %Vector for the SE values
+for k = 1:length(TimeVec)
+    K = P*C'*pinv(C*P*C' + R_dyn_m(:,:,k));
+    x = x + K*(y(:,k) - C*x);
+    P = (eye(6)-K*C)*P;
+    
+    x_est_loop(:,k) = x;                            %Save data from the Sensor fusion
+    
+    x = Ad*x + Bd*u(k);
+    P = Ad*P*Ad' + Q_dyn_m(:,:,k); %Gd*Q*Gd';
+
+end
+
+%% Plot
+figure('Name','Real flight vs estimation Self calculated');
+plot(TimeVec,h);
+grid on;
+hold on;
+plot(TimeVec,x_est_loop(1,:)); 
+plot(X_estimatd.time,X_estimatd.signals.values(:,1));
+plot(TimeVec,v);
+plot(TimeVec,x_est_loop(2,:));
+plot(TimeVec,a);
+plot(TimeVec,x_est_loop(3,:));
+plot(TimeVec,p);
+plot(TimeVec,x_est_loop(4,:));
+hold off;
+legend('real Height','estiamted Height','estimated Height Simulink','real Speed','estimated Speed','real acceloration','estimated acceloration','real pressure','estimated pressure');
+ylabel('height & accelaration');
+xlabel('Time [s]');
