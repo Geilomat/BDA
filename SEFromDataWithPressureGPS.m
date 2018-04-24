cb44f60cf4527cf15046d1c8dadfcbcdfcfc596c
Unknown <michikurmann@hotmail.com>
2018-04-24 21:48:19 +0200

    Added Measurment Noise Calculater just Variance

--- SEFromDataWithPressureGPS.m
+++ SEFromDataWithPressureGPS.m
@@ -9,19 +9,19 @@ clear all; close all;
 % x1 = Height, x2 = Velocity,x3 = Acceloration, x4 = Pressure, x5 = Temp,
 % x6 = Temp_dot
 % Tempteratur at start = 15 C° -> 288.15 K°
-load('PressLookUp.mat');
+load('PressLookUp.mat')
 
 A = [0 1 0 0 0 0;0 0 1 0 0 0; 0 0 0 0 0 0;0 PressLookUp(1,1) 0 0 0 PressLookUp(2,1);0 0 0 0 0 1;0 0 0 0 0 0];
 B = [0;0;0;0;0;0];                %No direct input
 C = [1 0 0 0 0 0;0 0 1 0 0 0;0 0 0 1 0 0;0 0 0 1 0 0;0 0 0 0 1 0];          %Output ist Height and Acceleration
 D = [0];
-G = [0;0;1;1;0;1];                %System noise only on acceloration
+G = [0 0 0;0 0 0;1 0 0 ;0 1 0;0 0 0;0 0 1];                %System noise only on acceloration
 
 
 
 % Get the time vector from the ARIS simulation.
 %load('TimeFromHassan.mat');
-load('TimeSimu.mat');
+load('TimeSimu.mat')
 % Interpolate to get to 1 ms sampling time.
 TimeVec = t';
 Tau = TimeVec(end)/length(TimeVec);
@@ -47,7 +47,7 @@ disp(['Size of A: ' num2str(size(A))]);
 disp(['Rank: ' num2str(rank([C;C*Ad;C*Ad*Ad]))]);
 
 %% Read sensor Data
-load('StateSimu.mat');
+load('StateSimu.mat')
 
 % Get the heigth vector from the ARIS simulation.
 h = state(:,3)';
@@ -68,9 +68,6 @@ a = diff(v)/(Tau);
 % Ad zeros to maintain vector length
 v = [v 0];
 a = [a 0 0];
-
-%v = interp(v,5);
-%a = interp(a,5);
 
 % get height of GPS by deleting engouh values so it becomes 5Hz sample rate
 % and then ad Zero Order Hold to get static value
@@ -130,17 +127,47 @@ hold off;
 
 %% Add noise to sensor data
 
-VART = 0.002;
-VARP1 = 0.2950;
-VARP2 = 0.4950;
-VARA = 7.1177e-07;
-VARGPS = 0.1;
+T_var_brn = 8.4040e-04;
+p_var_brn = 1.7034;
+p2_var_brn = p_var_brn * 2;
+p_var_upflight = 0.7034;
+p2_var_upflight = p_var_upflight * 2;
+a_var_brn = 0.0128;
+a_var_upflight = 0.0028;
+aofst_var_brn = 0.0001;
+GPS_var = 0.1;
 
-T_mes = T + randn(1,length(T)).*sqrt(VART);
-a_mes = a + randn(1,length(a)).*sqrt(VARA);
-p_mes_1 = p + randn(1,length(P1)).*sqrt(VARP1);
-p_mes_2 = p + randn(1,length(P2)).*sqrt(VARP2);
-h_mes_GPS = h_GPS + randn(1,length(h_GPS)).*sqrt(VARGPS);
+% with EPFL data
+%load('sensorNoiseTir2.mat')
+%load('sensorNoiseTir1.mat')
+
+% Temperatur
+T_mes = T + randn(1,length(T)).*sqrt(T_var_brn);
+
+% Acceleloration > Add a offset and more noise if while the motor is
+% burning
+a_mes = zeros(1,length(a));
+a_offset = 4;
+for k = 1:length(a)
+    if a(k) > 20
+    a_mes(k) = a(k) + randn * sqrt(a_var_brn) + (a_offset + randn*aofst_var_brn);
+    else
+    a_mes(k) = a(k) + randn * sqrt(a_var_upflight) + (a_offset + randn*aofst_var_brn);    
+    end
+end
+
+% Pressure -> more noise if the motor is burning
+for k = 1:length(a)
+    if a(k) > 20
+    p_mes_1 = p + randn(1,length(P1)).*sqrt(p_var_brn);
+    p_mes_2 = p + randn(1,length(P2)).*sqrt(p2_var_brn);
+    else
+    p_mes_1 = p + randn(1,length(P1)).*sqrt(p_var_upflight);
+    p_mes_2 = p + randn(1,length(P2)).*sqrt(p2_var_upflight);    
+    end
+end
+% GPS
+h_mes_GPS = h_GPS + randn(1,length(h_GPS)).*sqrt(GPS_var);
 
 % T_mes = awgn(T,40,'measured');
 % h_mes_GPS = awgn(h_GPS,80,'measured');
@@ -151,11 +178,15 @@ h_mes_GPS = h_GPS + randn(1,length(h_GPS)).*sqrt(VARGPS);
 figure('Name','Noise Data');
 hold on;
 plot(h_mes_GPS);
+plot(h_GPS);
 plot(p_mes_1);
 plot(p_mes_2);
+plot(p)
 plot(a_mes);
+plot(a);
 plot(T_mes);
-legend('GPS','Pressure 1','Pressure 2','Acceloration Measured','Temperatur');
+plot(T);
+legend('GPSnoise','GPS','Pressure 1 noise','Pressure 2 noise','Pressure','Acceloration Measured','Acceleration','Temperatur noise','Temperatur');
 hold off;
 
 %% Create Noise Matrices
@@ -223,7 +254,8 @@ ACEL = [100 100 100 50 30 ones(1,length(TimeVec)-5)*20];
 PRE = [zeros(1,10) ones(1,length(TimeVec)-20)*0.7 zeros(1,10)];         %Due to linearization it gets less accurate in the middle
 TMP = ones(1,length(TimeVec))*0;
 DTMP = ones(1,length(TimeVec))*0.1;
-Q_dyn = [HGT;SPE;ACEL;PRE;TMP;DTMP];  
+%Q_dyn = [HGT;SPE;ACEL;PRE;TMP;DTMP]; 
+Q_dyn = [ACEL;PRE;DTMP];
 
 %Add all noise vectors into an noise matrix
 Q_dyn_m = diag(Q_dyn(:,1)');
@@ -277,7 +309,7 @@ for k = 1:length(TimeVec)
     x_est_loop(:,k) = x;                            %Save data from the Sensor fusion
     
     x = Ad*x + Bd*u(k);
-    P = Ad*P*Ad' + Q_dyn_m(:,:,k); %Gd'* Q_dyn_m(:,:,k)*Gd; %Gd*Q*Gd';
+    P = Ad*P*Ad' + Gd* Q_dyn_m(:,:,k)*Gd'; %Gd*Q*Gd';
 
 end
 disp('...finished!');
@@ -301,7 +333,126 @@ legend('real Height','estiamted Height','estimated Height Simulink','real Speed'
 ylabel('height & speed & accelaration & pressure');
 xlabel('Time [s]');
 
+
+
 %% Loop 2
+% Initalize the System
+
+% x1 = Height, x2 = Velocity,x3 = Acceloration,x4 = aoffset x5 = Pressure
+% x6 = Temp, x7 = Temp_dot
+% Tempteratur at start = 15 C° -> 288.15 K°
+load('PressLookUp.mat');
+
+A = [0 1 0 0 0 0 0;
+    0 0 1 0 0 0 0;
+    0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0;
+    0 PressLookUp(1,1) 0 0 0 0 PressLookUp(2,1);
+    0 0 0 0 0 0 1;
+    0 0 0 0 0 0 0];
+B = [0;0;0;0;0;0;0];                %No direct input
+C = [1 0 0 0 0 0 0;
+    0 0 1 -1 0 0 0;
+    0 0 0 0 1 0 0;
+    0 0 0 0 1 0 0;
+    0 0 0 0 0 1 0];          %Output ist Height, Pressures and Temperature
+D = [0];
+G = [0 0 0 0;
+    0 0 0 0;
+    1 0 0 0;
+    0 1 0 0;
+    0 0 1 0;
+    0 0 0 0;
+    0 0 0 1];                %System noise on acceloration, acc offset, pressure an Tdot
+
+Sys = ss(A,B,C,D,Tau)
+
+ %Diskretierung der Systemmatritzen
+Ad = expm(A*Tau);     
+Gd = Ad * G;
+Bd = Ad * B;
+
+% Dummy vairable for Simualation
+simin = zeros(1,length(TimeVec));
+
+disp(['Size of A: ' num2str(size(A))]);
+
+%% Observability test;
+disp(['Rank: ' num2str(rank([C;C*Ad;C*Ad*Ad]))]);
+
+%% Add Offset to system noise
+%Static System noise:
+HGT = 0;
+SPE = 0;
+ACEL = 70;
+ACELOFS = 0.01;
+PRE = 0.1;
+TMP = 0;
+DTMP = 0.1;
+Q = diag([ACEL;ACELOFS;PRE;TMP;DTMP]);
+
+%Dynamic Sytem noise:
+HGT = ones(1,length(TimeVec))*0;
+SPE = ones(1,length(TimeVec))*0;
+ACEL = [100 100 100 50 30 ones(1,length(TimeVec)-5)*10];
+ACELOFS = ones(1,length(TimeVec))*ACELOFS;
+PRE = [zeros(1,10) ones(1,length(TimeVec)-20)*0.7 zeros(1,10)];         %Due to linearization it gets less accurate in the middle
+TMP = ones(1,length(TimeVec))*0;
+DTMP = ones(1,length(TimeVec))*0.1;
+%Q_dyn = [HGT;SPE;ACEL;PRE;TMP;DTMP]; 
+Q_dyn = [ACEL;ACELOFS;PRE;DTMP];
+
+%Add all noise vectors into an noise matrix
+Q_dyn_m = diag(Q_dyn(:,1)');
+for n = 2:length(TimeVec)
+    Q_dyn_m = cat(3,Q_dyn_m,diag(Q_dyn(:,n)'));
+end
+
+%% Do estimation Loop
+
+u = zeros(1,length(TimeVec));                       %Input vector is zero
+y = [h_mes_GPS;a_mes;p_mes_1;p_mes_2;T_mes];        %Output are the measurements
+y_t = timeseries(y,TimeVec);
+x = [0;0;0;0;Po;T(1);0];                            %Start Vector should be like this
+P = eye(7);                                         %Standart can maybe be increased
+
+x_est_loop2 = zeros(size(x,1),length(TimeVec));      %Vector for the SE values
+disp('Loop 2 start..');
+for k = 1:length(TimeVec)
+    K = P*C'*pinv(C*P*C' + R_dyn_m(:,:,k));
+    x = x + K*(y(:,k) - C*x);
+    P = (eye(7)-K*C)*P;
+    
+    x_est_loop2(:,k) = x;                            %Save data from the Sensor fusion
+    
+    x = Ad*x + Bd*u(k);
+    P = Ad*P*Ad' + Gd* Q_dyn_m(:,:,k)*Gd'; %Gd*Q*Gd';
+
+end
+disp('...finished!');
+
+%% Plot Loop 2
+
+figure('Name','Real flight 2 vs estimation Self calculated');
+plot(TimeVec,h);
+grid on;
+hold on;
+plot(TimeVec,x_est_loop2(1,:)); 
+plot(TimeVec,x_est_loop(1,:));
+plot(TimeVec,v);
+plot(TimeVec,x_est_loop2(2,:));
+plot(TimeVec,a);
+plot(TimeVec,x_est_loop2(3,:));
+plot(TimeVec,x_est_loop2(4,:));
+plot(TimeVec,p);
+plot(TimeVec,x_est_loop2(5,:));
+hold off;
+legend('real Height','estiamted Height','estimated Height Simulink','real Speed','estimated Speed','real acceloration','estimated acceloration','acceleration offset','real pressure','estimated pressure');
+ylabel('height & speed & accelaration & pressure');
+xlabel('Time [s]');
+
+
+%% Loop 3
 A = [0 1 0 0;0 0 1 0; 0 0 0 0;-0.00649 0 0 0];
 B = [0;0;0;0];                %No direct input
 C = [1 0 0 0;1 0 0 0;1 0 0 0;0 0 1 0;0 0 0 1];          %Output ist Height and Acceleration
@@ -332,8 +483,8 @@ Height2 = 0;
 Temp = T(1);
 
 
-x_est_loop2 = zeros(size(x,1),length(TimeVec));      %Vector for the SE values
-disp('Loop 2 start..');
+x_est_loop3 = zeros(size(x,1),length(TimeVec));      %Vector for the SE values
+disp('Loop 3 start..');
 for k = 1:length(TimeVec)
     K = P*C'*pinv(C*P*C' + R_dyn_m(:,:,k));
     Height1 = CalcHeight(Po,p_mes_1(k),T_mes(k),0,true);
@@ -342,7 +493,7 @@ for k = 1:length(TimeVec)
     x = x + K*([h_mes_GPS(k);Height1;Height2;a_mes(k);Temp] - C*x);
     P = (eye(4)-K*C)*P;
     
-    x_est_loop2(:,k) = x;                            %Save data from the Sensor fusion
+    x_est_loop3(:,k) = x;                            %Save data from the Sensor fusion
     
     x = Ad*x + Bd*u(k);
     P = Ad*P*Ad' + Q_dyn_m(:,:,k); %Gd*Q*Gd';
@@ -352,18 +503,18 @@ disp('...finished!');
 
 %% Plot
 
-figure('Name','Real flight 2 vs estimation Self calculated');
+figure('Name','Real flight 3 vs estimation Self calculated');
 plot(TimeVec,h);
 grid on;
 hold on;
-plot(TimeVec,x_est_loop2(1,:)); 
+plot(TimeVec,x_est_loop3(1,:)); 
 plot(TimeVec,x_est_loop(1,:));
 plot(TimeVec,v);
-plot(TimeVec,x_est_loop2(2,:));
+plot(TimeVec,x_est_loop3(2,:));
 plot(TimeVec,a);
-plot(TimeVec,x_est_loop2(3,:));
+plot(TimeVec,x_est_loop3(3,:));
 plot(TimeVec,p);
-plot(TimeVec,x_est_loop2(4,:));
+plot(TimeVec,x_est_loop3(4,:));
 hold off;
 legend('real Height','estiamted Height','estimated Height Simulink','real Speed','estimated Speed','real acceloration','estimated acceloration','real pressure','estimated pressure');
 ylabel('height & speed & accelaration & pressure');
@@ -371,20 +522,23 @@ xlabel('Time [s]');
 
 %% Difference between estimation and ground truth
 diff = abs(h-x_est_loop(1,:));
-display(['Loop 1 Max difference:' num2str(max(diff)) ' min difference:' num2str(min(diff)) ' average difference:' num2str(sum(diff)/length(diff))]);
+display(['Loop 1 "Normal" Max difference:' num2str(max(diff)) ' min difference:' num2str(min(diff)) ' average difference:' num2str(sum(diff)/length(diff))]);
 diff = abs(h-x_est_loop2(1,:));
-display(['Loop 2 Max difference:' num2str(max(diff)) ' min difference:' num2str(min(diff)) ' average difference:' num2str(sum(diff)/length(diff))]);
+display(['Loop 2 Acceloration with offset Max difference:' num2str(max(diff)) ' min difference:' num2str(min(diff)) ' average difference:' num2str(sum(diff)/length(diff))]);
+diff = abs(h-x_est_loop3(1,:));
+display(['Loop 3 Pressure as height Max difference:' num2str(max(diff)) ' min difference:' num2str(min(diff)) ' average difference:' num2str(sum(diff)/length(diff))]);
 
-%%
-figure('Name','Generated sensor data');
-hold on;
-grid on;
-plot(TimeVec,h);
-plot(TimeVec,h_GPS)
-plot(TimeVec,a);
-plot(TimeVec,T)
-plot(TimeVec,p);
-legend('Real height in z','GPS heigt in z','Acceloration','Temperature','Pressure');
-hold off;
-ylabel('Height [m], Accelaration [m/s^2], Temperature [K°], Pressure [hPa]');
-xlabel('Time [s]');
+
+% %%
+% figure('Name','Generated sensor data');
+% hold on;
+% grid on;
+% plot(TimeVec,h);
+% plot(TimeVec,h_GPS)
+% plot(TimeVec,a);
+% plot(TimeVec,T)
+% plot(TimeVec,p);
+% legend('Real height in z','GPS heigt in z','Acceloration','Temperature','Pressure');
+% hold off;
+% ylabel('Height [m], Accelaration [m/s^2], Temperature [K°], Pressure [hPa]');
+% xlabel('Time [s]');
--- SEwithEPFLData.m
+++ SEwithEPFLData.m
@@ -1,29 +1,29 @@
 clear all; clc; close all; % remove that
-
+% Sensordata
+% seqNmbr time altitude airspeed pressure temp ax ay az gx gy gz gx gy gz
+% GPS data
+% empty time number of sats hdop lat lon altitude
+hasGPS = false;
 
 log_time = [];     % Time of IMO sample: synch. with gyro/accel Nx[s]
-log_data = []; 
-log_imu_a = [];     % Accelerometer: Accels    ax, ay, az  Nx3*[m/s^2]
-log_imu_g = [];     % Gyroscope, Angular rates gx, gy, gz  Nx3*[rad/s]
+log_imu_a = [];     % Accelerometer: Accels    ax, ay, az  Nx3*[g]
+log_imu_g = [];     % Gyroscope, Angular rates gx, gy, gz  Nx3*[°/s]
 log_temp  = [];     % Temperature?
-log_press = [];     % Pressure sensor:[time[s], pressure[Pa/m^2], airspeed]
-% log_pos   = [];     % Nx[time  xpos     ypos     zpos        zpos2]
+log_press = [];     % Pressure sensor:[time[s], pressure[hPa], airspeed]
 log_h     = [];     % Nx[time altituded]
 log_airspeed = [];  % [airspeed]
-log_gps   = [];
-% log_vel   = [];     % Nx[time  xvel     yvel     zvel        zvel2]
-% log_rot   = [];     % Nx[time  rot.v.x  rot.v.y  rot.v.z     rot.s]
-% log_b_sensors_noise = true; % true if sensors contain noise
-
-% i=0;
 
 % Open log file uncomment for different Flights.
-% EPFL Tir_test3D_fix;
 %load('Tir_test3d2_Paths.mat');
 %load('Tir_test3d1_Paths.mat');
 %load('18_11_18_Eric_Paths.mat');
 load('18_11_18_Greg_Paths.mat');
 
+if hasGPS
+    log_GPS_time = [];
+    log_GPS_pos = [];
+    log_GPS_sat = [];
+end
 
 for k = 1:size(Paths);
     
@@ -57,36 +57,7 @@ end
             log_press = [log_press; data(5)];
             log_temp = [log_temp; data(6)];
             log_imu_a = [log_imu_a; data(7) data(8) data(9)];
-            log_imu_g = [log_imu_g; data(10) data(11) data(12)];
-            
-%             % TODO: Data types checks, handle formatting errors
-%             if sample_id=='g'      % Gyroscope data
-%                 log_imu_t = [log_imu_t; sample_time];
-%                 log_imu_g = [log_imu_g; data(3), data(4), data(5)];
-%             elseif sample_id=='a'  % Accelerometer data (always after gyro)
-%                 if not(sample_time==log_imu_t(end))
-%                     error('IMU error: Gyro&Accel not synchronized?!');
-%                 else
-%                     log_imu_a = [log_imu_a; data(3), data(4), data(5)];
-%                 end
-%             elseif sample_id=='t'
-%                 log_temp = [log_temp; data(1), data(3), data(4)];    
-%             elseif sample_id=='p' % can denote both position and pressure...
-%                 if num==6 % Position   %  time     xpos    ypos   zpos  zpos2
-%                     log_pos = [log_pos; data(1), data(3), data(4), data(5), data(6)];
-%                 elseif num == 4 % Temperature
-%                     log_press = [log_press; data(1), data(3), data(4)];
-%                 else
-%                     error('Didn''manage to read ''p'' line of logfile');
-%                 end
-%             elseif sample_id=='h' %  time     press    alti   altispeed
-%                 log_h = [log_h; data(1), data(3), data(4), data(5)];  
-%             elseif sample_id=='v' %  time     xvel    yvel   zvel  zvel2
-%                 log_vel = [log_vel; data(1), data(3), data(4), data(5), data(6)];  
-%             elseif sample_id=='r' %  time    rot.v.x  rot.v.y  rot.v.z  rot.s
-%                 log_rot = [log_rot; data(1), data(3), data(4), data(5), data(6)];    
-%             end
-           
+            log_imu_g = [log_imu_g; data(10) data(11) data(12)];     
             end
      end
 %   end
@@ -94,19 +65,189 @@ end
 fclose(fid);
 end
 
-%% Adjust time vector;
+if hasGPS
+for k = 1:size(PathsGPS);
+    
+fid = fopen(PathsGPS(k,:), 'r');
+if fid == -1
+   error('Cannot open file: %s', FileName);
+end
+
+     isEOB = false;
+     iData = 0;
+     while ~isEOB && ~feof(fid)
+        % Read new line
+        Line = fgetl(fid);
+        if ~ischar(Line) || strncmp(Line, 'EOB', 3)
+           isEOB = true;
+        else
+            % Read line of logfile
+            [data, num, err, ind1] = sscanf(Line, '%f %f %f %f %f %f', 6);
+            %sample_time = data(2); 
+            %if(sample_time > 185 & sample_time < 300)
+            log_GPS_time = [log_GPS_time; data(1)];
+            log_GPS_sat = [log_GPS_sat;data(2)];
+            log_GPS_pos = [log_GPS_pos; data(3) data(4) data(5) data(6)];     
+            end
+     end
+
+fclose(fid);
+end
+startTime = log_GPS_time(1);
+log_GPS_time(:) = log_GPS_time(:)-startTime;
+log_GPS_time(:) = log_GPS_time(:)/1000000;
+end
+
+% Adjust time vector;
 startTime = log_time(1);
 log_time(:) = log_time(:)-startTime;
 log_time(:) = log_time(:)/1000000;
+
+
 %%
-clear all;
-Paths(1,:) = '../SensorData/EPFLData/18.11.2017.2 - Greg/telemetry_data_1_1511012308145';
-Paths(2,:) = '../SensorData/EPFLData/18.11.2017.2 - Greg/telemetry_data_2_1511012328160';
-Paths(3,:) = '../SensorData/EPFLData/18.11.2017.2 - Greg/telemetry_data_3_1511012347617';
-Paths(4,:) = '../SensorData/EPFLData/18.11.2017.2 - Greg/telemetry_data_4_1511012369307';
-Paths(5,:) = '../SensorData/EPFLData/18.11.2017.2 - Greg/telemetry_data_5_1511012401268';
-%Paths(6,:) = '../SensorData/EPFLData/18.11.2017.1 - Eric/telemetry/flight data/telemetry_data_6_1511008823759';
-%Paths(7,:) = '../SensorData/EPFLData/18.11.2017.1 - Eric/telemetry/flight data/telemetry_data_7_1511008838265';
-%Paths(8,:) = '../SensorData/EPFLData/18.11.2017.1 - Eric/telemetry/flight data/telemetry_data_8_1511008909814';
-%Paths(9,:) = '../SensorData/EPFLData/18.11.2017.1 - Eric/telemetry/flight data/telemetry_data_1_1511008752975';
-%Paths(6,:) = '../SensorData/EPFLData/24.02.2018.1 - Tir_test3D/sensors_data_005_1519479278352';
+plot(log_time,log_h);
+hold on;
+%plot(log_time,log_press);
+%plot(log_GPS_time, log_GPS_pos(:,4)+22);
+
+%% Testing
+
+%plot(log_time,log_imu_g)
+plot(log_time,log_imu_a);
+legend('1','2','3');
+hold on;
+%hold on; 
+%plot(log_time,log_h)
+
+
+%figure('Name','Accel without/with gyro')
+angle = zeros(length(log_time),1);
+%angle(1) = 0;
+for k = 2:length(log_time)
+    dT = log_time(k) - log_time(k-1);
+    angle(k) = angle(k-1) + (log_imu_g(k,3) -log_imu_g(k-1,3))*dT;
+end
+
+az = log_imu_a(:,2) .* -cos(angle*pi/180)*9.81;
+
+%plot(log_time,log_imu_a(:,1)*9.81);
+%hold on;
+plot(log_time,az);
+%plot(log_time,log_h);
+%legend('without gyro','with gyro','height');
+
+%% Find Icongnition Time und Burnduration:
+
+T_ico_ind = 1; %Icogntiono Time index
+while az(T_ico_ind) < 20
+   T_ico_ind = T_ico_ind + 1; 
+end
+
+T_ico = log_time(T_ico_ind); %Icogniotion time
+
+T_brn_ind = T_ico_ind; %Burnout time index
+while az(T_brn_ind) > 10
+    T_brn_ind = T_brn_ind +1;
+end
+
+T_brn = log_time(T_brn_ind); %Burnout time
+
+
+T_par_ind = T_brn_ind; % Parachute time index
+
+while abs(az(T_par_ind)) < 10.1
+   T_par_ind = T_par_ind + 1; 
+end
+
+T_par = log_time(T_par_ind); % Parachute time
+
+
+
+%% Calculate Mean an Variance before Icognition, during Burntime and during ubpflight until parachute activation from Accelerometer:
+
+decayTime = 3;
+a_mean = sum(az(1:T_ico_ind-decayTime))/(T_ico_ind-decayTime);
+a_var_preIco  = sum((az(1:T_ico_ind-decayTime)-a_mean).^2)/(T_ico_ind-decayTime-1);
+
+% fing the best polynom to recreate curve during burntime:
+riseTime = 9;
+decayTime = 3;
+p_brn = polyfit(log_time(T_ico_ind+riseTime:T_brn_ind-decayTime),az(T_ico_ind+riseTime:T_brn_ind-decayTime),2);
+p_brn_curve = polyval(p_brn,log_time(T_ico_ind+riseTime:T_brn_ind-decayTime));
+a_var_brn = sum((az(T_ico_ind+riseTime:T_brn_ind-decayTime)-p_brn_curve).^2)/(T_brn_ind-decayTime-(T_ico_ind+riseTime)-1);
+
+%For checking purpose:
+plot(log_time(T_ico_ind+riseTime:T_brn_ind-decayTime),az(T_ico_ind+riseTime:T_brn_ind-decayTime),log_time(T_ico_ind+riseTime:T_brn_ind-decayTime),p_brn_curve);
+
+% fing the best polynom to recreate curve during upflight:
+riseTime = 20;
+decayTime = 12;
+p_upflight = polyfit(log_time(T_brn_ind+riseTime:T_par_ind-decayTime),az(T_brn_ind+riseTime:T_par_ind-decayTime),2);
+p_upflight_curve =polyval(p_upflight,log_time(T_brn_ind+riseTime:T_par_ind-decayTime));
+a_var_upflight = sum((az(T_brn_ind+riseTime:T_par_ind-decayTime)-p_upflight_curve).^2)/(T_par_ind-decayTime-(T_brn_ind+riseTime)-1);
+
+%For checking purpose:
+plot(log_time(T_brn_ind+riseTime:T_par_ind-decayTime),az(T_brn_ind+riseTime:T_par_ind-decayTime),log_time(T_brn_ind+riseTime:T_par_ind-decayTime),p_upflight_curve);
+
+%% Same for Barometer pressure:
+
+decayTime = 3;
+p_mean = sum(log_press(1:T_ico_ind-decayTime))/(T_ico_ind-decayTime);
+p_var_preIco  = sum((log_press(1:T_ico_ind-decayTime)-p_mean).^2)/(T_ico_ind-decayTime-1);
+
+%find the best polynom to recreate curve during burntime:
+riseTime = 6;
+decayTime = 3;
+p_brn = polyfit(log_time(T_ico_ind+riseTime:T_brn_ind-decayTime),log_press(T_ico_ind+riseTime:T_brn_ind-decayTime),2);
+p_brn_curve = polyval(p_brn,log_time(T_ico_ind+riseTime:T_brn_ind-decayTime));
+p_var_brn = sum((log_press(T_ico_ind+riseTime:T_brn_ind-decayTime)-p_brn_curve).^2)/(T_brn_ind-decayTime-(T_ico_ind+riseTime)-1);
+
+%For checking purpose:
+plot(log_time(T_ico_ind+riseTime:T_brn_ind-decayTime),log_press(T_ico_ind+riseTime:T_brn_ind-decayTime),log_time(T_ico_ind+riseTime:T_brn_ind-decayTime),p_brn_curve);
+
+%find the best polynom to recreate curve during upflight:
+riseTime = 3;
+decayTime = 20;
+p_upflight = polyfit(log_time(T_brn_ind+riseTime:T_par_ind-decayTime),log_press(T_brn_ind+riseTime:T_par_ind-decayTime),2);
+p_upflight_curve =polyval(p_upflight,log_time(T_brn_ind+riseTime:T_par_ind-decayTime));
+p_var_upflight = sum((log_press(T_brn_ind+riseTime:T_par_ind-decayTime)-p_upflight_curve).^2)/(T_par_ind-decayTime-(T_brn_ind+riseTime)-1);
+
+%For checking purpose:
+plot(log_time(T_brn_ind+riseTime:T_par_ind-decayTime),log_press(T_brn_ind+riseTime:T_par_ind-decayTime),log_time(T_brn_ind+riseTime:T_par_ind-decayTime),p_upflight_curve);
+
+
+%% Same for Barometer Temperature:
+
+decayTime = 3;
+T_mean = sum(log_temp(1:T_ico_ind-decayTime))/(T_ico_ind-decayTime);
+T_var_preIco  = sum((log_temp(1:T_ico_ind-decayTime)-T_mean).^2)/(T_ico_ind-decayTime-1);
+
+%find the best polynom to recreate curve during burntime:
+riseTime = 6;
+decayTime = 3;
+p_brn = polyfit(log_time(T_ico_ind+riseTime:T_brn_ind-decayTime),log_temp(T_ico_ind+riseTime:T_brn_ind-decayTime),2);
+p_brn_curve = polyval(p_brn,log_time(T_ico_ind+riseTime:T_brn_ind-decayTime));
+T_var_brn = sum((log_temp(T_ico_ind+riseTime:T_brn_ind-decayTime)-p_brn_curve).^2)/(T_brn_ind-decayTime-(T_ico_ind+riseTime)-1);
+
+%For checking purpose:
+plot(log_time(T_ico_ind+riseTime:T_brn_ind-decayTime),log_temp(T_ico_ind+riseTime:T_brn_ind-decayTime),log_time(T_ico_ind+riseTime:T_brn_ind-decayTime),p_brn_curve);
+
+%find the best polynom to recreate curve during upflight:
+riseTime = 3;
+decayTime = 2;
+p_upflight = polyfit(log_time(T_brn_ind+riseTime:T_par_ind-decayTime),log_temp(T_brn_ind+riseTime:T_par_ind-decayTime),2);
+p_upflight_curve =polyval(p_upflight,log_time(T_brn_ind+riseTime:T_par_ind-decayTime));
+T_var_upflight = sum((log_temp(T_brn_ind+riseTime:T_par_ind-decayTime)-p_upflight_curve).^2)/(T_par_ind-decayTime-(T_brn_ind+riseTime)-1);
+
+%For checking purpose:
+plot(log_time(T_brn_ind+riseTime:T_par_ind-decayTime),log_temp(T_brn_ind+riseTime:T_par_ind-decayTime),log_time(T_brn_ind+riseTime:T_par_ind-decayTime),p_upflight_curve);
+
+%% Path generation
+%clear all;
+%hasGPS = true;
+%PathsGPS(1,:) = '../SensorData/EPFLData/24.02.2018.1 - Tir_test3D/gps_data_004_1519469099934';
+%PathsGPS(2,:) = '../SensorData/EPFLData/24.02.2018.2-Tir_test3D_fix/gps_data_001_1519479278350';
+% PathsGPS(3,:) = '../SensorData/EPFLData/18.11.2017.2 - Greg/telemetry_data_3_1511012347617';
+% PathsGPS(4,:) = '../SensorData/EPFLData/18.11.2017.2 - Greg/telemetry_data_4_1511012369307';
+% PathsGPS(5,:) = '../SensorData/EPFLData/18.11.2017.2 - Greg/telemetry_data_5_1511012401268';
+
